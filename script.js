c1237e3150c75f3fa491801bc0dee5640f7b2d8a
shariful07 <sharifislam5@gmail.com>
2026-03-07 21:59:05 +0600

    add login functionality

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hello');
+const loginBtn = document.getElementById('login-btn');
+loginBtn.addEventListener('click', () => {
+    const userInp = document.getElementById('username-inp');
+    const userName = userInp.value;
+
+    const passwordInp = document.getElementById('password-inp');
+    const password = passwordInp.value;
+
+    if (userName === 'admin' && password === 'admin123') {
+        window.location.href = 'all.html';
+    }
+
+});
